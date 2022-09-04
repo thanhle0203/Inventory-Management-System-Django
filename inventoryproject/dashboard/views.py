87b66a512243acf627175c84c53d8dab56f9e975
Thanh Le <thanhlevan0203@gmail.com>
2022-09-04 00:16:08 -0700

    Installed Views & URLs in Dashboard page

--- inventoryproject/dashboard/views.py
+++ inventoryproject/dashboard/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('<h1 style="color:red;"> This is the index Page </h1>')
+
+def staff(request):
+    return HttpResponse('This is the staff Page')
