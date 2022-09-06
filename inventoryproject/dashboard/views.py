7a89d57ef8908472ff24dab4e5aff76d083fa3b2
Thanh Le <thanhlevan0203@gmail.com>
2022-09-05 23:21:38 -0700

    Created Templates Pages

--- inventoryproject/dashboard/views.py
+++ inventoryproject/dashboard/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1 style="color:red;"> This is the index Page </h1>')
+    return render(request, 'index.html')
 
 def staff(request):
-    return HttpResponse('This is the staff Page')
+    return render(request, 'staff.html')
