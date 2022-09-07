1db145710596da1fa36ee5561368c15ff3e775e4
Thanh Le <thanhlevan0203@gmail.com>
2022-09-07 01:37:54 -0700

    Build staff page & home page

--- inventoryproject/inventoryproject/settings.py
+++ inventoryproject/inventoryproject/settings.py
@@ -110,7 +110,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+
+STATTICFILES_DIRS = [
+    BASE_DIR / "static"
+]
+
+STATIC_ROOT = (BASE_DIR/"asert/")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
