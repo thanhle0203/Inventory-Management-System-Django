03e4c544c7222103a59abf89f53b5dae8cbdd76e
Thanh Le <thanhlevan0203@gmail.com>
2022-09-08 03:01:57 -0700

    Created user app

--- inventoryproject/dashboard/admin.py
+++ inventoryproject/dashboard/admin.py
@@ -7,6 +7,9 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'quantity')
     list_filter = ['category']
 
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('Product', 'Staff', 'Order quantity')
+
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
 admin.site.register(Order)
