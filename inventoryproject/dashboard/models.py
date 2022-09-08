696e68b3dca51f4367b828ebb0f7576c41d23224
Thanh Le <thanhlevan0203@gmail.com>
2022-09-07 23:33:25 -0700

    Created Product model database

--- inventoryproject/dashboard/models.py
+++ inventoryproject/dashboard/models.py
@@ -1,3 +1,16 @@
+from sre_constants import CATEGORY
+from statistics import quantiles
+from unicodedata import category, name
 from django.db import models
 
 # Create your models here.
+CATEGORY = (
+    ('Stationary', 'Stationary'),
+    ('Electronics', 'Electronics'),
+    ('Food', 'Food'),
+)
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY, null=True)
+    quantity = models.PositiveIntegerField(null=True)
+
