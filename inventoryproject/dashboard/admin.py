0699aef5750638271a123edb2335763ff839bd41
Thanh Le <thanhlevan0203@gmail.com>
2022-09-08 00:38:20 -0700

    modified admin tuple

--- inventoryproject/dashboard/admin.py
+++ inventoryproject/dashboard/admin.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import Group
 admin.site.site_header = 'DjangoIntentory Dashboard'
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'quantity')
-    list_filter = ('category',)
+    list_filter = ['category']
 
 # Register your models here.
 admin.site.register(Product, ProductAdmin)
