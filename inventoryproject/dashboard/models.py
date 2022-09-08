ae46810665e7570a0ed0d4e430a893e24be472fb
Thanh Le <thanhlevan0203@gmail.com>
2022-09-08 00:00:40 -0700

    Format Product model in Admin Page

--- inventoryproject/dashboard/models.py
+++ inventoryproject/dashboard/models.py
@@ -14,3 +14,6 @@ class Product(models.Model):
     category = models.CharField(max_length=20, choices=CATEGORY, null=True)
     quantity = models.PositiveIntegerField(null=True)
 
+    def __str__(self):
+        return f'{self.name}-{self.category}-{self.quantity}'
+
