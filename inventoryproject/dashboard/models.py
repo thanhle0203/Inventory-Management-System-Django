bf21327f37b22054a6743ea67ca7256a6ba9b661
Thanh Le <thanhlevan0203@gmail.com>
2022-09-08 00:50:38 -0700

    Created Order model

--- inventoryproject/dashboard/models.py
+++ inventoryproject/dashboard/models.py
@@ -2,6 +2,7 @@ from sre_constants import CATEGORY
 from statistics import quantiles
 from unicodedata import category, name
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 CATEGORY = (
@@ -16,4 +17,13 @@ class Product(models.Model):
 
     def __str__(self):
         return f'{self.name}-{self.category}-{self.quantity}'
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    staff = models.ForeignKey(User, models.CASCADE, null=True)
+    order_quantity = models.PositiveIntegerField(null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.product} ordered by {self.staff.username}'
 
