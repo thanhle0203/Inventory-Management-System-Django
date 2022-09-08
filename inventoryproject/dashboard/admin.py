19c66691d15be909b75928b0ba778de9a6e8619c
Thanh Le <thanhlevan0203@gmail.com>
2022-09-08 00:02:34 -0700

    Taken out Group in Admin Page

--- inventoryproject/dashboard/admin.py
+++ inventoryproject/dashboard/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Product
+from django.contrib.auth.models import Group
 
 # Register your models here.
 admin.site.register(Product)
+admin.site.unregister(Group)
