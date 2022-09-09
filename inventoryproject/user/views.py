48b58f6b0da40933575f260eabc8649e35772aa2
Thanh Le <thanhlevan0203@gmail.com>
2022-09-08 19:55:19 -0700

    Installed crispy forms

--- inventoryproject/user/views.py
+++ inventoryproject/user/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 
@@ -8,6 +8,7 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save();
+            return redirect('')
     else:
         form = UserCreationForm()
 
