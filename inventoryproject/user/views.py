91d472ce01f8e2df09e83c4c35ab77bc53460051
Thanh Le <thanhlevan0203@gmail.com>
2022-09-08 19:08:25 -0700

    Added scrispy form

--- inventoryproject/user/views.py
+++ inventoryproject/user/views.py
@@ -3,8 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def register(request):
-    form = UserCreationForm()
-    context = {
-        'form':form
-    }
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save();
+    else:
+        form = UserCreationForm()
+        context = {
+            'form':form
+        }
+    
     return render(request, 'user/register.html', context)
