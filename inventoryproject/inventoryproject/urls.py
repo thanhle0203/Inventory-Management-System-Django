9cc1f70f170bda9e662f610eff46326ce2d7b181
Thanh Le <thanhlevan0203@gmail.com>
2022-09-10 14:57:06 -0700

    Created Customer Profile Page

--- inventoryproject/inventoryproject/urls.py
+++ inventoryproject/inventoryproject/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('dashboard.urls')),
     path('register/', user_views.register,  name='user-register'),
+    path('profile/', user_views.profile,  name='user-profile'), 
     path('', auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='user-logout'),
+    
 ]
--- inventoryproject/user/views.py
+++ inventoryproject/user/views.py
@@ -18,3 +18,6 @@ def register(request):
     }
     
     return render(request, 'user/register.html', context)
+
+def profile(request):
+    return render(request, 'user/profile.html')
