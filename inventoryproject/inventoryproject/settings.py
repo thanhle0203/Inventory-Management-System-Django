2c59d08764e9587b27a6d8c8ebae55be24f20a97
Thanh Le <thanhlevan0203@gmail.com>
2022-09-10 15:26:28 -0700

    Created User Profile link

--- inventoryproject/inventoryproject/settings.py
+++ inventoryproject/inventoryproject/settings.py
@@ -125,6 +125,9 @@ LOGIN_REDIRECT_URL = 'dashboard-index'
 
 LOGIN_URL = 'user-login'
 
+MEDIA_ROOT = ('BASE_DIR' / 'media' )
+
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/dev/ref/settings/#default-auto-field
