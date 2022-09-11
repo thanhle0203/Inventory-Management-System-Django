84be70e655a991c520ca191562f220760bd5feac
Thanh Le <thanhlevan0203@gmail.com>
2022-09-11 15:19:02 -0700

    Created Order Form

--- inventoryproject/dashboard/forms.py
+++ inventoryproject/dashboard/forms.py
@@ -1,9 +1,14 @@
 from dataclasses import fields
 from pyexpat import model
 from django import forms
-from .models import Product
+from .models import Order, Product
 
 class ProductForm(forms.ModelForm):
     class Meta: 
         model = Product
         fields = ['name', 'category', 'quantity']
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ['product', 'order_quantity']
