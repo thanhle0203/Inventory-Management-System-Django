650c426adab7a7f6fa0f80c28db7cc9b46bb0e7d
Thanh Le <thanhlevan0203@gmail.com>
2022-09-10 20:44:35 -0700

    Update User Profile

--- inventoryproject/user/forms.py
+++ inventoryproject/user/forms.py
@@ -2,8 +2,7 @@ from dataclasses import field
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-
-from inventoryproject.user.models import Profile
+from .models import Profile
 
 
 class CreateUserForm(UserCreationForm):
