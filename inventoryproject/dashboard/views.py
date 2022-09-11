e130a9c448e1afe7a1a54cf6253d133cf56bcd5a
Thanh Le <thanhlevan0203@gmail.com>
2022-09-11 16:35:02 -0700

    Created Charts and Data VIsualization for Orders & Products

--- inventoryproject/dashboard/views.py
+++ inventoryproject/dashboard/views.py
@@ -12,6 +12,8 @@ from django.contrib import messages
 @login_required
 def index(request):
     orders = Order.objects.all()
+    products = Product.objects.all()
+
     if request.method == 'POST':
         form = OrderForm(request.POST)
         if form.is_valid():
@@ -25,6 +27,7 @@ def index(request):
     context = {
         'orders': orders,
         'form': form,
+        'products': products,
     }
     return render(request, 'dashboard/index.html', context)
 
