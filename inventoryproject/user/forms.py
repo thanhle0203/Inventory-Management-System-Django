741a0815fa177c40bd2d6f6b77e64e851bbc8c17
Thanh Le <thanhlevan0203@gmail.com>
2022-09-10 18:40:44 -0700

    Created Update User Profile Form

--- inventoryproject/user/forms.py
+++ inventoryproject/user/forms.py
@@ -1,6 +1,9 @@
+from dataclasses import field
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+
+from inventoryproject.user.models import Profile
 
 
 class CreateUserForm(UserCreationForm):
@@ -9,4 +12,18 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['address', 'phone', 'image']
+
+
+                
 
--- inventoryproject/user/views.py
+++ inventoryproject/user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
-from .forms import CreateUserForm
+from .forms import CreateUserForm, UserUpdateForm, ProfileUpdateForm
 
 
 # Create your views here.
@@ -23,7 +23,16 @@ def profile(request):
     return render(request, 'user/profile.html')
 
 def profile_update(request):
+    if request.method=='POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+    else:
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
     context = {
-        
+        'user_form': user_form,
+        'profile_form': profile_form,
     }
-    return render(request, 'user/profile_update')
+    return render(request, 'user/profile_update.html', context)
+
