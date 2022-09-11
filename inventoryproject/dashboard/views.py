f777065f15ad94b000e53ac73462ba46938d69e0
Thanh Le <thanhlevan0203@gmail.com>
2022-09-10 23:55:47 -0700

    get product details in Product Page

--- inventoryproject/dashboard/views.py
+++ inventoryproject/dashboard/views.py
@@ -15,7 +15,8 @@ def staff(request):
 
 @login_required
 def product(request):
-    items = Product.objects.all()
+    #items = Product.objects.all()
+    items = Product.objects.raw('SELECT * FROM dashboard_product')
 
     context = {
         'items': items,
