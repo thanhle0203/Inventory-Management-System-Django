198d344356cf6d996bffb26cd6a14d0ad4dbfaaf
Thanh Le <thanhlevan0203@gmail.com>
2022-09-12 22:22:07 -0700

    Created URL reset password Page

--- inventoryproject/inventoryproject/urls.py
+++ inventoryproject/inventoryproject/urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     path('profile/update/', user_views.profile_update,  name='user-profile-update'),  
     path('', auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='user-logout'),
-    
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
