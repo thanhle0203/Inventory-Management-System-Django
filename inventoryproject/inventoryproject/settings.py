cc204f7dd48b7e71be04959101f319d977d25a1f
Thanh Le <thanhlevan0203@gmail.com>
2022-09-15 14:41:54 -0700

    new updated

--- inventoryproject/inventoryproject/settings.py
+++ inventoryproject/inventoryproject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-jhrwr@7-jt^no74#n2uurigmulz_u#7h4_b00jg)w)o(8w6avu"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
